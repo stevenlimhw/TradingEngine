02dfa0154dc010ea0feb07bdf61229885abf4592
Steven Lim <limhwsteven@gmail.com>
2023-06-01 11:01:45 +0800

    Add ServiceProvider

--- Program.cs
+++ Program.cs
@@ -6,3 +6,8 @@ using Microsoft.Extensions.Hosting;
 using TradingEngineServer.Core;
 
 using var engine = TradingEngineServerHostBuilder.BuildTradingEngineServer();
+TradingEngineServerServiceProvider.ServiceProvider = engine.Services;
+{
+    using var scope = TradingEngineServerServiceProvider.ServiceProvider.CreateScope();
+    await engine.RunAsync().ConfigureAwait(false);
+}
